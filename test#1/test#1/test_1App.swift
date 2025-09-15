eba4458cfef7ccf1e1faa84cc927d3076a6e90a9
Maia Munich <maia.munich@gmail.com>
2025-09-14 20:37:30 -0400

    commiting test#1 to see if github is connected

---
+++ test#1/test#1/test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  test_1App.swift
+//  test#1
+//
+//  Created by Maia Munich on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
